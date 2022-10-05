361b48f802ce827d06836b943a40a6115966734e
crushon9 <catco929@naver.com>
2022-10-05 14:38:31 +0900

    hi

--- Web10_MVC/src/main/java/edu/web/board/util/PageCriteria.java
+++ Web10_MVC/src/main/java/edu/web/board/util/PageCriteria.java
@@ -6,13 +6,13 @@ package edu.web.board.util;
 public class PageCriteria {
 	private int page; // 현재 페이지 번호
 	private int numsPerPage; // 한 페이지의 게시글 개수
-	
+
 	public PageCriteria() {
 		// 생성자 생성시 기본값
 		this.page = 1;
 		this.numsPerPage = 5;
 	}
-	
+
 	public PageCriteria(int page, int numsPerPage) {
 		this.page = page;
 		this.numsPerPage = numsPerPage;
@@ -34,19 +34,14 @@ public class PageCriteria {
 	public void setNumsPerPage(int numsPerPage) {
 		this.numsPerPage = numsPerPage;
 	}
-	
+
 	// 현재 보여지는 페이지의 시작 글 일련번호(rn)
 	public int getStart() {
 		return (this.page - 1) * this.numsPerPage + 1;
 	}
-	
+
 	// 현재 보여지는 페이지의 마지막 글 일련번호(rn)
 	public int getEnd() {
 		return this.page * this.numsPerPage;
 	}
-	
-	
 }
-
-
-
